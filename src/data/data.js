fcc9cdf8d75fefc09781f2336c58f1b8d93023cc
nikhil <nikhilbhagat2293@gmail.com>
2023-05-27 18:35:27 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,26 @@
+export const data = [
+  {
+    id: 1,
+    name: "Movie App",
+    image:
+      "https://user-images.githubusercontent.com/96339472/226867560-b0c989c7-a9c1-4e38-bbd1-3c13e253e853.png",
+    github: "https://github.com/NikhilBhagat01/MovieApp",
+    live: "https://movie-app-rouge-seven.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "FactOpedia",
+    image:
+      "https://user-images.githubusercontent.com/96339472/213453163-577eb681-f022-4969-be92-c0691f494c4f.png",
+    github: "https://github.com/NikhilBhagat01/FactOpedia",
+    live: "https://fact-opedia.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "Internshala Clone",
+    image:
+      "https://user-images.githubusercontent.com/96339472/241150149-c4b4e384-46fc-48d6-a064-2315f9604a7e.png",
+    github: "https://github.com/NikhilBhagat01/internshala-clone",
+    live: "https://rich-pear-narwhal-ring.cyclic.app/",
+  },
+];
