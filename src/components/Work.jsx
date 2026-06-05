681575218c9a96de4081b4c1f23c14ac33ce7c21
Nikhil Bhagat <96339472+NikhilBhagat01@users.noreply.github.com>
2026-06-06 01:22:42 +0530

    Remove comments and fix text in Work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { data } from "../data/data.js";
 
 const Work = () => {
-  // projects file
+ 
   const project = data;
-  //setProject(data);
+  
 
   return (
     <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
@@ -13,7 +13,7 @@ const Work = () => {
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
             Work
           </p>
-          <p className="py-6">// Check out some of my recent work</p>
+          <p className="py-6">Check out some of my recent work</p>
         </div>
 
         {/* container for projects */}
